05cfa46d44646ca38dcb946be30bdaeeec29f78d
parwezshariq <shariqparwez@outlook.com>
2020-07-04 15:39:46 +0530

    Groovy Shell, Groovy Scripts

---
+++ additionalFiles/Sample.groovy
@@ -0,0 +1,8 @@
+/*
+
+println "Welcome to Groovy!"
+
+*/
+
+println "Welcome to Groovy!"
+println "Today's date is " + new Date()
---
+++ src/SampleGroovy.groovy
@@ -0,0 +1,12 @@
+/*
+
+println "Welcome to Groovy... from IntelliJ!"
+println "Today's date is " +  new Date().toString()
+
+ */
+
+println "Welcome to Groovy... from IntelliJ!"
+println "Today's date is " +  new Date().toString()
+
+def sum = 3 + 5
+println "The sum is " + sum
