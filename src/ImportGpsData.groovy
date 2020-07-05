a1702f6ecbd621d5c754efa2789b9f8d350852f4
parwezshariq <shariqparwez@outlook.com>
2020-07-05 10:57:03 +0530

    Working with Grape - Dependency Management

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -56,6 +56,7 @@ for (point in gpx.rte.rtept) {
 }
 */
 
+/*
 // Reading XML properties as list
 def file = new File('../data/fells_loop.gpx')
 println file.exists()
@@ -73,4 +74,35 @@ gpx.rte.rtept.each {
     println it.@lat
     println it.@lon
     println it.time
+}
+*/
+
+// Format time
+
+// Adding external dependency using Grapes, taken from mvnrepository to import joda-time
+@Grapes(
+        @Grab(group='joda-time', module='joda-time', version='2.5')
+)
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+def file = new File('../data/fells_loop.gpx')
+println file.exists()
+
+def slurper = new XmlSlurper()
+def gpx = slurper.parse(file)
+println gpx.name
+println ''
+println gpx.desc
+println ''
+println gpx.@version
+println gpx.@creator
+
+gpx.rte.rtept.each {
+    println it.@lat
+    println it.@lon
+
+    def printableTime = new DateTime(it.time.toString())
+    def format = DateTimeFormat.forPattern('MM/dd/yyyy - hh:mm aa')
+    println printableTime.toString(format)
 }
