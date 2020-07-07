1f3887d392589ae5ee974ecb954d5ca9be13a3d4
parwezshariq <shariqparwez@outlook.com>
2020-07-07 21:22:08 +0530

    Unit Testing Application

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -77,6 +77,8 @@ gpx.rte.rtept.each {
 }
 */
 
+/*
+
 // Format time
 
 // Adding external dependency using Grapes, taken from mvnrepository to import joda-time
@@ -105,4 +107,24 @@ gpx.rte.rtept.each {
     def printableTime = new DateTime(it.time.toString())
     def format = DateTimeFormat.forPattern('MM/dd/yyyy - hh:mm aa')
     println printableTime.toString(format)
+}
+*/
+
+def file = new File('../data/fells_loop.gpx')
+
+def slurper = new XmlSlurper()
+def gpx = slurper.parse(file)
+println gpx.name
+println ''
+println gpx.desc
+println ''
+println gpx.@version
+println gpx.@creator
+
+gpx.rte.rtept.each {
+    println it.@lat
+    println it.@lon
+
+    def parser = new DateParser()
+    println parser.parse(it.time.toString())
 }
