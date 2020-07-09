594d8f6887caa3645a7e982c3f32b15a9de8b550
parwezshariq <shariqparwez@outlook.com>
2020-07-09 22:15:12 +0530

    Querying and Updating Data

--- src/database/QueryRoutepoints.groovy
+++ src/database/QueryRoutepoints.groovy
@@ -7,6 +7,21 @@ import groovy.sql.Sql
 def sql = Sql.newInstance("jdbc:mysql://localhost:3306/gps", "root", "root", 
 	"com.mysql.jdbc.Driver")
 
+/*
+// To Fetch all rows
+sql.eachRow('select * from routepoints') {
+	println "Latitude: ${it.latitude}, Longitude: ${it.longitude}, " +
+		"Timestep: ${it.timestep}, Temperature: ${it.temperature}"
+}
+*/
+
+/*
+// To Fetch one rows
+def row = sql.firstRow('select latitude, longitude from routepoints')
+println "Latitude: ${row.latitude}, Longitude: ${row.longitude}"
+*/
+
+// To update row(s)
 def newTemperature = 100
 sql.executeUpdate("update routepoints set temperature = ${newTemperature}")
 
